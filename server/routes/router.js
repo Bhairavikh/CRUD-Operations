a71748fec9921bd297d9713d850534c38f61e48c
bhairavikhairnar <bhairavi.suresh@ecerasystem.com>
2021-05-16 22:20:41 +0530

    CRUD operations

---
+++ server/routes/router.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const route = express.Router();
+const controller = require('../controller/controller');
+
+const axios = require('axios');
+
+route.get('/', (req, res) => {
+    axios.get('http://localhost:3000/api/users')
+        .then(function (response) {
+            
+            res.render('index', { users: response.data });
+        })
+        .catch(err => {
+            res.send(err);
+        })
+});
+
+route.get('/add-user', (req, res) => {
+    res.render('add_user');
+});
+
+route.get('/update-user', (req, res) => {
+    axios.get('http://localhost:3000/api/users', { params: { id: req.query.id } })
+        .then(function (userdata) {
+            res.render("update_user", { user: userdata.data })
+        })
+        .catch(err => {
+            res.send(err);
+        })
+    
+});
+
+//api
+route.post('/api/users', controller.create);
+route.get('/api/users', controller.find);
+route.put('/api/users/:id', controller.update);
+route.delete('/api/users/:id', controller.delete);
+
+
+module.exports = route
